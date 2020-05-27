c3ce522064db9127afb961adaeb1fa8136ee9027
RAHUL BIR <rahulbir0001@gmail.com>
2020-05-27 22:11:16 +0530

    add sountry details

--- country_app/urls.py
+++ country_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('',views.home,name='home')
-
-
+    path('',views.home,name='home'),
+    path('country/<int:cid>/', views.country, name='country'),
+    path('details/<int:cid>/', views.details, name='details'),
 ]
--- country_app/views.py
+++ country_app/views.py
@@ -10,3 +10,26 @@ def home(request):
         'continent':continent
     }
     return render(request,'index.html',context)
+
+def country(request,cid):
+    continent = Continent.objects.all()
+    cat = Continent.objects.get(pk=cid)
+
+    country=Country.objects.filter(continent=cat)
+
+    context = {
+        'country': country,
+        'continent': continent
+    }
+    return render(request, 'index.html', context)
+
+def details(request,cid):
+    country = Country.objects.filter(pk=cid)
+    context = {
+        'country':country,
+
+    }
+    return render(request, 'details.html', context)
+
+
+
